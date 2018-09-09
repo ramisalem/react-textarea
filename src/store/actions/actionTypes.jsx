1726233acae32c1545496e5d170abbb282cf2b3e
Rami salem <you@example.com>
2018-09-09 18:11:03 +0530

    adding three actions for the Posting the text

--- src/store/actions/actionTypes.jsx
+++ src/store/actions/actionTypes.jsx
@@ -1,13 +1,10 @@
-export const  START      = 'START';
+export const  START             = 'START';
 export const  TEXT_CHANGED      = 'TEXT_CHANGED';
-// export const  TEXT_POST_SART    = 'TEXT_POST_SART';
-// export const  TEXT_POST_SUCCESS = 'TEXT_POST_SUCCESS';
-// export const  TEXT_POST_FAIL    = 'TEXT_POST_FAIL';
+export const  POST_TEXT_SART    = 'POST_TEXT_SART';
+export const  POST_TEXT_SUCCESS = 'POST_TEXT_SUCCESS';
+export const  POST_TEXT_FAIL    = 'POST_TEXT_FAIL';
 
 
-// export const  TEST_FETCHING_START = 'TEST_FETCHING_START';
-// export const  TEXT_FETCHED = 'TEXT_FETCHED';
-// export const TEXT_FETCHED_FAIL = 'TEXT_FETCHED_FAIL';
 
 export const AUTH_START = 'AUTH_START';
 export const AUTH_SUCCESS = 'AUTH_SUCCESS';
