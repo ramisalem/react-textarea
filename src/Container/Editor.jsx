8c89ad888cd33830e1d9a6713ca1441298dd624d
Rami salem <you@example.com>
2018-09-12 17:13:36 +0530

    making the params from the  edit state

--- src/Container/Editor.jsx
+++ src/Container/Editor.jsx
@@ -6,11 +6,11 @@ import * as actionCreators from '../store/actions/index';
 import axios from 'axios';
 
 class Editor  extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      value: ''
+      value: '' ,
+     
     };
     this.handleChange = this.handleChange.bind(this);
   }
@@ -21,55 +21,53 @@ class Editor  extends Component {
      this.setState({
        value: event.target.value 
      });
-     let mystate = this.props.text ;
-      let  bodyFormData = new FormData();
-      bodyFormData.set('the_post' ,  "لاكن" )
-    axios({
-      method: 'post',
-      url: 'http://test.dhad.me/spellcheck/postajax/',
-      data: bodyFormData,
-      config: { headers: {'Content-Type': 'multipart/form-data' }}
-      })
-      .then(function (response) {
-          //handle success
-          //console.log(response);
-          console.log('full path ' , response.data.raw_correctionsList)
-      })
-      .catch(function (response) {
-          //handle error
-          console.log(response);
-      });
+     let  bodyFormData = new FormData();
+     let curret = this.props.text ;
+     bodyFormData.set('the_post' ,  this.props.text )
+     axios({
+       method: 'post',
+       url: 'http://test.dhad.me/spellcheck/postajax/',
+       data: bodyFormData,
+       config: { headers: {'Content-Type': 'multipart/form-data' }}
+       })
+       .then(res => {
+           console.log(res.data.raw_errList);
+     })
+       .catch(function (response) {
+           //handle error
+           console.log(response);
+       });
      
   }
   
   componentWillMount() {
-     //console.log(this.props.onChangeText);
+     
      this.props.onStart();
      this.props.onChangeText(this.state.value);
   }
   componentDidMount() {
-
-   
+    
+      
   }
   
   render() {
+    
     return (
-      <div> 
-         <h1>{this.state.value}</h1>
+      <div className="cont"> 
       <form>
              <Input.TextArea
+              className="my-text" 
               size="large"
               value={this.state.value}
               placeholder="start typing now "
               onChange={this.handleChange}
-              rows={8}
-              cols={40}
+              rows={20}
+              cols={80}
              /> 
-      </form>
-      </div>
+      </form>   
+    </div>
     );
   }
-
 }
 
 const mapStateToProps = state => {
@@ -82,7 +80,6 @@ const mapDispatchToProps = dispatch => {
   return {
       onStart: () => dispatch(actionCreators.start()),
       onChangeText: (value)   => dispatch(actionCreators.TextChange(value)) 
-     
       
   }
 };
